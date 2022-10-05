4f4e0a265f0d63610ed8283fab27ebbc730cddaa
Eric Eldredge <lettertwo@gmail.com>
2022-10-05 13:05:33 -0400

    Make actions bindable to occurrences

--- lua/occurrency/Action.lua
+++ lua/occurrency/Action.lua
@@ -1,5 +1,6 @@
 local ACTION = "__ACTION__"
 local Occurrence = require("occurrency.Occurrence")
+local log = require("occurrency.log")
 
 -- A callable type that can be used as a keymap callback.
 -- It can be sequenced with other actions via the `+` operator.
@@ -19,6 +20,52 @@ function Action.is_action(candidate)
   return type(candidate) == "table" and candidate.type == ACTION
 end
 
+-- Create a new action from a callback or existing action.
+-- If the `callback` is a function, the new action will wrap it.
+-- If the `callback` is an action, the new action will extend it.
+-- If the `callback` is `nil`, the new action will extend the current action. This only works
+-- for existing actions.
+---@param callback? (fun(...): nil) | OccurrencyAction
+---@return OccurrencyAction
+function Action:new(callback)
+  local action = { type = ACTION }
+  local meta = self
+  if callback == nil then
+    assert(self.callback, "Action must have a callback")
+  elseif self.is_action(callback) then
+    -- If the callback is an action, we just extend it.
+    ---@cast callback -function
+    meta = callback
+  elseif type(callback) ~= "function" then
+    error("callback must be a function")
+  else
+    action.callback = callback
+  end
+
+  return setmetatable(action, { __index = meta, __add = meta.add, __call = meta.call })
+end
+
+-- Binds an existing action to the given arguments.
+-- The first argument is expected to be an `Occurrence`.
+-- This is useful for creating actions within actions,
+-- e.g., adding keymaps to perform additional actions with an occurrence.
+---@param occurrence Occurrence
+---@param ... any
+---@return OccurrencyAction
+function Action:bind(occurrence, ...)
+  local args = select("#", ...) > 0 and { ... } or nil
+  local bound = self:new()
+  function bound:call(...)
+    if args then
+      return self.callback(occurrence, unpack(args), ...)
+    else
+      return self.callback(occurrence, ...)
+    end
+  end
+  getmetatable(bound).__call = bound.call
+  return bound
+end
+
 ---@param other OccurrencyAction | fun(occurrence: Occurrence, ...): any
 function Action:add(other)
   if type(other) == "function" or self.is_action(other) then
@@ -32,22 +79,6 @@ end
 ---@param occurrence? Occurrence
 function Action:call(occurrence, ...)
   return self.callback(occurrence or Occurrence:new(), ...)
-end
-
--- Convert a function into an action.
----@param callback (fun(...): nil) | OccurrencyAction
----@return OccurrencyAction
-function Action:new(callback)
-  if self.is_action(callback) then
-    ---@cast callback -function
-    return callback
-  end
-
-  if type(callback) ~= "function" then
-    error("callback must be a function")
-  end
-
-  return setmetatable({ type = ACTION, callback = callback }, { __index = self, __add = self.add, __call = self.call })
 end
 
 -- Convert a table of functions into a table of actions.
