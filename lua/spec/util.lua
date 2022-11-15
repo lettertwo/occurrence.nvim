c3f4472e155ca7223275f30f9315268011a4948e
Eric Eldredge <lettertwo@gmail.com>
2022-11-15 13:03:31 -0500

    Add a spec util for creating a test buffer

---
+++ lua/spec/util.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+local DEFAULT_CONTENT = {
+  "this is default content that has been defaultly contented",
+  "to repeately include content by default. The content features",
+  "some default occurrences of content by default.",
+}
+local DEFAULT_FILETYPE = "text"
+
+-- Creates a new buffer with content for testing.
+---@param content? string | string[]
+---@param filetype? string
+---@return number buffer
+function M.buffer(content, filetype)
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_option(buf, "filetype", filetype or DEFAULT_FILETYPE)
+  vim.api.nvim_command("buffer " .. buf)
+  if type(content) == "string" then
+    content = vim.split(content, "\n")
+  else
+    content = content or DEFAULT_CONTENT
+  end
+  vim.api.nvim_buf_set_lines(0, 0, -1, true, content)
+  return buf
+end
+
+return M
